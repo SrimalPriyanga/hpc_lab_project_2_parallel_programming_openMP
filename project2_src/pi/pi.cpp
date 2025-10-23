a07cf0d3c1d303be0f2fd122eb0d511a5254a56f
Srimal Priyanga <s.priyanga22@gmail.com>
2025-10-23 14:56:01 +0200

    Implement serial and parallel versions of Pi computation in pi.cpp

--- project2_src/pi/pi.cpp
+++ project2_src/pi/pi.cpp
@@ -13,6 +13,11 @@ int main(int argc, char *argv[]) {
   /* TODO Serial Version of Computing Pi*/
   time = omp_get_wtime();
   // TODO Fill this gap
+  // : Implement the serial version of pi computation
+  for (long int i = 0; i < N; ++i) {
+    double x = (i + 0.5) * dx;
+    sum += 4.0 / (1.0 + x * x);
+  }
   pi = sum * dx;
   time = omp_get_wtime() - time;
   printf("pi=%e, N=%ld, time_srl=%e secs\n", pi, N, time);
@@ -22,6 +27,12 @@ int main(int argc, char *argv[]) {
   time = omp_get_wtime();
   sum = 0.;
   // TODO Fill this gap
+  // : Use OpenMP to parallelize this loop
+  #pragma omp parallel for reduction(+:sum)
+  for (long int i = 0; i < N; ++i) {
+      double x = (i + 0.5) * dx;
+      sum += 4.0 / (1.0 + x * x);
+  }
   pi = sum*dx;
   time = omp_get_wtime() - time;
   printf("pi=%e, N=%ld, time_plr=%e secs\n", pi, N, time);
